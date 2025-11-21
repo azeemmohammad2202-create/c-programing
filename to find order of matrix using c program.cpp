ef2c887218dc1e7f210dca36f5164d1b4e23fdd5
azeemmohammad2202-create <azeem.mohammad2202@gmail.com>
2025-11-21 22:01:03 +0530

    Add files via upload

---
+++ to find order of matrix using c program.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int A[10][10],B[10][10],C[10][10],i,j,r1,c1,r2,c2;
+	printf("enter order of matrixA:");
+	scanf("%d%d",&r1,&c1);
+	printf("enter order of matrixB:");
+	scanf("%d%d",&r2,&c2);
+	if((r1==r2))&&((c1==c2));
+	{
+		return 0;
+	}
+
