aca33ee7c1d272f8d6a9b822f2c3973414528ea0
azeemmohammad2202-create <azeem.mohammad2202@gmail.com>
2025-11-21 21:58:20 +0530

    Add files via upload

---
+++ transpose of a matix.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{ intm[10][10],n,i,j;
+	printf("enter order of matrix:");
+	scanf("%d",&n);
+	printf("enter the elements of matrix:\n");
+	for(i=0;i<n;i++)
+{
+	for(j=0;j<n;j++)
+	{
+
+    scanf("%d",&m[i][j]);
+}
+for(i=0;i<n;i++)
+{
+	for(j=i+1;)
+}
